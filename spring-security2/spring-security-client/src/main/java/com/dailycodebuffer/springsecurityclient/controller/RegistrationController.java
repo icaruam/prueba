8476cb174063b3df77bfa7e39daa12a923f15b59
icaruam <102391820+icaruam@users.noreply.github.com>
2022-04-24 02:49:58 -0500

    Add files via upload

---
+++ spring-security2/spring-security-client/src/main/java/com/dailycodebuffer/springsecurityclient/controller/RegistrationController.java
@@ -0,0 +1,41 @@
+package com.dailycodebuffer.springsecurityclient.controller;
+
+import com.dailycodebuffer.springsecurityclient.entity.User;
+import com.dailycodebuffer.springsecurityclient.event.RegistrationCompleteEvent;
+import com.dailycodebuffer.springsecurityclient.model.UserModel;
+import com.dailycodebuffer.springsecurityclient.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserModel userModel, final HttpServletRequest request) {
+        User user = userService.registerUser(userModel);
+        publisher.publishEvent(new RegistrationCompleteEvent(
+                user,
+                applicationUrl(request)
+        ));
+        return "Success";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        return "http://" +
+                request.getServerName() +
+                ":" +
+                request.getServerPort() +
+                request.getContextPath();
+    }
+
+}
---
+++ spring-security2/spring-security-client/src/main/java/com/dailycodebuffer/springsecurityclient/service/UserService.java
@@ -0,0 +1,14 @@
+package com.dailycodebuffer.springsecurityclient.service;
+
+
+import com.dailycodebuffer.springsecurityclient.entity.User;
+import com.dailycodebuffer.springsecurityclient.model.UserModel;
+
+
+public interface UserService  {
+
+    User registerUser(UserModel userModel);
+
+
+    void saveVerificationTokenForUser(String token, User user);
+}
